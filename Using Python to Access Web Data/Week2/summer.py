3bc04a279ad426aa23da1cf1105ff1c7413cf9e4
David Kemeza <dkemeza@gmail.com>
2015-11-06 20:43:37 -0500

    Working summer.py script, 100%

--- Using Python to Access Web Data/Week2/summer.py
+++ Using Python to Access Web Data/Week2/summer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import re
 from sys import argv
@@ -7,10 +7,12 @@ script, filename = argv
 
 txt = open(filename)
 
+values = re.findall('[0-9]+', txt.read())
+#print("Text:", txt.read())
+print("Values:", values)
+
 testing = [ '1', '2', '3', '4' ]
-
 newsum = 0
-
-for val in testing:
+for val in values:
     newsum += int(val)
 print("Sum:", newsum) 
