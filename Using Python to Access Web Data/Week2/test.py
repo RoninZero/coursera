18eee4f125ab091fc4946fbcd0e0462143c237f1
David Kemeza <dkemeza@gmail.com>
2015-11-06 19:59:11 -0500

    Complete test questions, 10/10

--- Using Python to Access Web Data/Week2/test.py
+++ Using Python to Access Web Data/Week2/test.py
@@ -9,3 +9,29 @@ str = "From: none@nowhere.none.net Sun Nov  2 20:01:15 2015"
 found = re.findall('^From: (\S+@\S+)', str)
 
 print(found)
+
+'''
+Question 1
+'''
+str1 = "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+found1 =  re.findall('@(\S+)', str1)
+print("Question 1:")
+print(found1)
+
+'''
+Question 8
+'''
+x = 'From: Using the : character'
+y = re.findall('^F.+:', x)
+print("Question 8:")
+print(y)
+
+'''
+Question 10
+'''
+str10 = "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+found10 =  re.findall('\S+?@\S+', str1)
+print("Question 10:")
+print(found10)
+
+
