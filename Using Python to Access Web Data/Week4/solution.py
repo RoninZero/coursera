f2fc739ff90c50d13675fc199bcea195a4bacfe5
David Kemeza <dkemeza@gmail.com>
2015-11-21 16:55:33 -0500

    add solution.py, 2nd assignment for this week

---
+++ Using Python to Access Web Data/Week4/solution.py
@@ -0,0 +1,28 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+
+import re
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+#tags = soup('a')
+sum = 0
+tags = soup('span')
+for tag in tags:
+   # Look at the parts of a tag
+   print 'TAG:',tag
+   print 'URL:',tag.get('href', None)
+   print 'Contents:',tag.contents[0]
+   print 'Attrs:',tag.attrs
+   #value = re.findall('[0-9]+', tag)
+   sum += int(tag.contents[0])
+
+
+print "Sum:", sum
