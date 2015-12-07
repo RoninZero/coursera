633e1b4d2d340d65bb9ac9d57d1374baae603cfc
David Kemeza <dkemeza@gmail.com>
2015-12-06 19:53:33 -0500

    Final working code, 100%

--- Using Python to Access Web Data/Week6/geojson.py
+++ Using Python to Access Web Data/Week6/geojson.py
@@ -21,10 +21,11 @@ while True:
         print data
         continue
 
-    print json.dumps(js, indent=4)
+    #print json.dumps(js, indent=4)
 
     lat = js["results"][0]["geometry"]["location"]["lat"]
     lng = js["results"][0]["geometry"]["location"]["lng"]
     print 'lat',lat,'lng',lng
     location = js['results'][0]['formatted_address']
     print location
+    print "Place id", js["results"][0]["place_id"]
