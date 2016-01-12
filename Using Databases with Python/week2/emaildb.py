f2122e4edd23a2a31f0d419cc6411b62487ad737
David Kemeza <dkemeza@gmail.com>
2016-01-12 09:36:27 -0500

    Weeks assignments complete 100%

--- Using Databases with Python/week2/emaildb.py
+++ Using Databases with Python/week2/emaildb.py
@@ -7,7 +7,7 @@ cur.execute('''
 DROP TABLE IF EXISTS Counts''')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = raw_input('Enter file name: ')
 if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
@@ -16,22 +16,25 @@ for line in fh:
     if not line.startswith('From: ') : continue
     pieces = line.split()
     email = pieces[1]
+    org = email.split('@')[1]
     print email
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email, ))
+    print org
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count) 
-                VALUES ( ?, 1 )''', ( email, ) )
-    else : 
-        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', 
-            (email, ))
-    # This statement commits outstanding changes to disk each 
-    # time through the loop - the program can be made faster 
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES ( ?, 1 )''', ( org, ) )
+    else :
+        cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?',
+            (org, ))
+    # This statement commits outstanding changes to disk each
+    # time through the loop - the program can be made faster
     # by moving the commit so it runs only after the loop completes
-    conn.commit()
+    #conn.commit()
 
+conn.commit()
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 print
 print "Counts:"
